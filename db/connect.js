e46d5cb0fbc904d2dbba8c8c989b748f627461bc
christopherleavitt99 <cml95@byui.edu>
2023-01-11 08:25:15 -0700

    changes

--- db/connect.js
+++ db/connect.js
@@ -1,35 +1,32 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const URI = "mongodb+srv://dbUser:dbUserPassword@cluster0.ipzyavp.mongodb.net/?retryWrites=true&w=majority"
-
-const connectDB = async()=>{
-  await mongoose.connect(URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  });
-  console.log('db connected..!');
-};
-
 const MongoClient = require('mongodb').MongoClient;
+
 let _db;
-const initDb = (callback) => {
+
+const initDb = callback => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
   MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
+    .then(client => {
       _db = client;
       callback(null, _db);
     })
-    .catch((err) => {
+    .catch(err => {
       callback(err);
     });
 };
+
 const getDb = () => {
   if (!_db) {
     throw Error('Db not initialized');
   }
   return _db;
 };
-module.exports = { initDb, getDb };
+
+module.exports = {
+  initDb,
+  getDb
+};
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const connectDB = require('./DB/Conneection');
+// const connectDB = require('./db/connect.js');
 const app = express();
 const mongodb = require('./db/connect');
 
-connectDB();
+// connectDB();
 const port = process.env.PORT || 3000;
 
-app.listen(Port, () => console.log('Server started'));
-app.use('/', require('./routes'));
+//app.listen(Port, () => console.log('Server started'));
+app.use('/', require('./routes')).use(express.json()).use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-mongodb.initDb((err) => {
+mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
   } else {
     app.listen(port);
     console.log(`Server is running on port ${port}`);
   }
+
 });
